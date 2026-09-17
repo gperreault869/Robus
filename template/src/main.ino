a1e678035e5e278ef32cd85ac22e7449e8b4843c
gperreault869 <gperreault869@gmail.com>
2026-09-17 10:06:07 -0400

    test

--- template/src/main.ino
+++ template/src/main.ino
@@ -12,7 +12,7 @@ Inclure les librairies de functions que vous voulez utiliser
 
 #include <LibRobus.h> // Essentielle pour utiliser RobUS
 
-
+//test
 
 /* ****************************************************************************
 Variables globales et defines
